497b60f0383dd9672e00c3ccdaf3b554b66a4347
Hadas Tamás <hadastk@gmail.com>
2025-02-24 17:50:54 +0100

    Add filter to saved games

--- src/app/pages/saved-games/saved-games.component.ts
+++ src/app/pages/saved-games/saved-games.component.ts
@@ -1,4 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, computed, model} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatAnchor, MatButton} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 import {RouterLink} from '@angular/router';
 import {BoardDTO} from '../../../types/board';
 import {initialSignal} from '../../utils/signals';
@@ -7,11 +12,26 @@ import {initialSignal} from '../../utils/signals';
   selector: 'app-saved-games',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    MatCardModule,
+    MatButton,
+    MatAnchor,
+    FormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput
   ],
   templateUrl: './saved-games.component.html',
   styleUrl: './saved-games.component.scss'
 })
 export default class SavedGamesComponent {
-  protected readonly games = initialSignal<BoardDTO[]>('games');
+  protected readonly allGames = initialSignal<BoardDTO[]>('games');
+  readonly filter = model<string>('');
+  protected readonly filteredGames = computed<BoardDTO[]>(() => this.#computeFilteredGames());
+
+  #computeFilteredGames() {
+    const regex = new RegExp(this.filter().trim(), 'i');
+
+    return this.allGames().filter(game => regex.test(game.name));
+  }
 }
