d686619b3d9d22581843a32921cae22a02b9c598
Hadas Tamás <hadastk@gmail.com>
2025-02-24 18:01:47 +0100

    Allow to remove game

--- src/app/pages/saved-games/saved-games.component.ts
+++ src/app/pages/saved-games/saved-games.component.ts
@@ -1,10 +1,12 @@
-import {Component, computed, model} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, computed, inject, model} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatAnchor, MatButton} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {RouterLink} from '@angular/router';
+import {firstValueFrom} from 'rxjs';
 import {BoardDTO} from '../../../types/board';
 import {initialSignal} from '../../utils/signals';
 
@@ -25,9 +27,24 @@ import {initialSignal} from '../../utils/signals';
   styleUrl: './saved-games.component.scss'
 })
 export default class SavedGamesComponent {
+  readonly #httpClient = inject(HttpClient);
+
   protected readonly allGames = initialSignal<BoardDTO[]>('games');
   readonly filter = model<string>('');
   protected readonly filteredGames = computed<BoardDTO[]>(() => this.#computeFilteredGames());
+
+  async remove(game: BoardDTO) {
+    try {
+      await firstValueFrom(this.#httpClient.delete(`http://localhost:5000/boards/${game.id}`));
+
+      const allGames = [...this.allGames()];
+      const index = allGames.indexOf(game);
+
+      allGames.splice(index, 1);
+      this.allGames.set(allGames);
+    } catch {
+    }
+  }
 
   #computeFilteredGames() {
     const regex = new RegExp(this.filter().trim(), 'i');
